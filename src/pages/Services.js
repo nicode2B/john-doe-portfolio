3092f5c1cd466d39b8907ba66ef6e9bd58785121
Dev.Nico <nicode.dev95@gmail.com>
2025-08-15 19:17:03 +0200

    avancé

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -8,18 +8,19 @@ export default function Services(){
     <>
       <Seo title="Services — John Doe" description="UX Design, Développement web, Référencement (SEO)." />
 
-      <div className="hero" style={{backgroundImage:`url(${banner})`, minHeight:'40vh'}}>
-        <div className="hero-content container"><h1>Mon offre de services</h1></div>
-      </div>
+      <div className="hero" style={{backgroundImage:`url(${banner})`, minHeight:'25vh'}}></div>
 
-      <p className="section-subtitle">Voici les prestations sur lesquelles je peux intervenir</p>
-      <div className="section-title"><div className="bar"></div></div>
+      <div className="section-title mb-5">
+        <h1>Mon offre de services</h1>
+        <p className="section-subtitle">Voici les prestations sur lesquelles je peux intervenir</p>
+        <div className="bar"></div>
+      </div>
 
       <Container className="pb-5">
         <Row className="gy-4">
-          <Col md={4}><ServiceCard icon="bi-pencil" title="UX Design">L’UX Design consiste à concevoir des produits en plaçant l’utilisateur au centre des préoccupations.</ServiceCard></Col>
-          <Col md={4}><ServiceCard icon="bi-code-slash" title="Développement web">Création de sites en HTML, CSS, JavaScript, PHP et frameworks (Bootstrap, React...).</ServiceCard></Col>
-          <Col md={4}><ServiceCard icon="bi-search" title="Référencement">Optimisation SEO pour améliorer la visibilité sur les moteurs de recherche.</ServiceCard></Col>
+          <Col md={4}><ServiceCard icon="bi-pencil" title="UX Design">L’UX Design est une dicipline qui consiste à concevoir des produits (sites web, applications mobiles, logiciels, objets connectés, etc.) en plaçant l’utilisateur au centre des préoccupations. L'objectif est de rendre l'expérience utilisateur la plus fluide et agréable possible</ServiceCard></Col>
+          <Col md={4}><ServiceCard icon="bi-code-slash" title="Développement web">Le développement de sites web consiste à Créer de sites internet en utilisant des langages de programmation ( HTML, CSS, JavaScript, PHP, etc. ) et des frameworks (Bootstrap, React, Agular, etc.).</ServiceCard></Col>
+          <Col md={4}><ServiceCard icon="bi-search" title="Référencement">Le référencement naturel (SEO) est une technique qui consiste à optimiser un site web pour le faire remonter dans les résultats de recherches (Google, Bing, Yahoo, etc.). L'objectif est d'attirer un maximum de visiteurs qualifiés sur le site</ServiceCard></Col>
         </Row>
       </Container>
     </>
